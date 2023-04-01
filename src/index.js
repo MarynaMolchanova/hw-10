3bae4dc3f0a559705ea704234760c9e2c08d5081
MarynaMolchanova <108984910+MarynaMolchanova@users.noreply.github.com>
2023-04-01 13:19:06 +0300

    home work

---
+++ src/index.js
@@ -0,0 +1,83 @@
+import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchCountries } from './fetchCountries';
+
+const debounce = require('lodash.debounce');
+const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  searchCountry: document.querySelector('#search-box'),
+  list: document.querySelector('.country-list'),
+  div: document.querySelector('.country-info'),
+};
+
+refs.searchCountry.addEventListener(
+  'input',
+  debounce(onGetCountry, DEBOUNCE_DELAY)
+);
+
+function onGetCountry(event) {
+  updatePage();
+  event.preventDefault();
+  let countryName = event.target.value.trim();
+
+  if (!countryName) {
+    return;
+  }
+  fetchCountries(countryName)
+    .then(data => {
+      if (data.length > 10) {
+        onWorn();
+      } else if (data.length === 1) {
+        getCountryInfo(data);
+      } else {
+        getCountriesList(data);
+      }
+    })
+    .catch(error => {
+      onError();
+    });
+}
+
+function getCountryTemplate(countries) {
+  return `<li class="country-item">
+        <img src="${countries.flags.svg}" alt="Flag of ${countries.name.official}" width="40" height ="40"/>
+        <p class = "country-name">${countries.name.official}</p>
+      </li>`;
+}
+function getCountriesList(countries) {
+  const countriesList = countries.map(getCountryTemplate).join('');
+
+  updatePage(countriesList, '');
+}
+function getCountryInfoTemplate(countries) {
+  return `<div class="country-wrap">
+        <img src="${countries.flags.svg}" alt="Flag of ${
+    countries.name.official
+  }" width="40" height ="40"/>
+        <p class = "country-name">${countries.name.official}</p>
+      </div>
+  <p class="country-capital"><b>Capital:</b> ${countries.capital}</p>
+      <p class="country-population"><b>Population:</b> ${
+        countries.population
+      }</p>
+      <p class="country-languages"><b>Languages:</b> ${Object.values(
+        countries.languages
+      ).join(', ')}</p>`;
+}
+function getCountryInfo(countries) {
+  const countryInfo = countries.map(getCountryInfoTemplate).join('');
+
+  updatePage('', countryInfo);
+}
+function onWorn() {
+  Notify.info('Too many matches found. Please enter a more specific name');
+}
+function onError() {
+  Notify.failure('Oops, there is no country with that name');
+}
+
+function updatePage(markupList = '', markupDiv = '') {
+  refs.list.innerHTML = markupList;
+  refs.div.innerHTML = markupDiv;
+}
